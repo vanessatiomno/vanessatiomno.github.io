b65363f8799d99f681aff528d6e1465ab2dd145e
mateus <vanessa@brandgym.com.br>
2019-09-27 13:58:37 -0300

    including new functionalities to cases

--- js/read-more.js
+++ js/read-more.js
@@ -3,13 +3,23 @@ let contextoShortText = document.querySelector('#contexto-short-text');
 let contextoLongText = document.querySelector('#contexto-long-text');
 let btnContextoReadMode = document.querySelector('#contexto-read-more');
 let contextoDots = document.querySelector('#contexto-dots');
+let lerMenosContexto = document.querySelector('#contexto-menos');
 
 btnContextoReadMode.addEventListener('click', function(){
     contextoShortText.classList.add('hidden');
     contextoLongText.classList.remove('hidden');
     contextoDots.classList.add('hidden');
     this.classList.add('hidden');
-})
+    lerMenosContexto.classList.remove('hidden');
+});
+
+lerMenosContexto.addEventListener('click', function(){
+    contextoShortText.classList.remove('hidden');
+    contextoLongText.classList.add('hidden');
+    contextoDots.classList.remove('hidden');
+    btnContextoReadMode.classList.remove('hidden');
+    this.classList.add('hidden');
+});
 
 
 //leia mais da solução
@@ -18,10 +28,20 @@ let solucaoShortText = document.querySelector('#solucao-short-text');
 let solucaoLongText = document.querySelector('#solucao-long-text');
 let btnSolucaoReadMode = document.querySelector('#solucao-read-more');
 let solucaoDots = document.querySelector('#solucao-dots');
+let lerMenosSolucao = document.querySelector('#solucao-menos');
 
 btnSolucaoReadMode.addEventListener('click', function(){
     solucaoShortText.classList.add('hidden');
     solucaoLongText.classList.remove('hidden');
     solucaoDots.classList.add('hidden');
+    this.classList.add('hidden');
+    lerMenosSolucao.classList.remove('hidden');
+});
+
+lerMenosSolucao.addEventListener('click', function(){
+    solucaoShortText.classList.remove('hidden');
+    solucaoLongText.classList.add('hidden');
+    solucaoDots.classList.remove('hidden');
+    btnSolucaoReadMode.classList.remove('hidden');
     this.classList.add('hidden');
 })
