4fdf677ac09dd0c31808d44e7dfb1e4670772786
mateus <vanessa@brandgym.com.br>
2019-10-02 09:53:45 -0300

    changing cases styles

--- js/menu.js
+++ js/menu.js
@@ -1,41 +1,54 @@
-let btnMenu = document.querySelector('#nav-icon');
+let buttonMenu = document.querySelector('#nav-icon');
 let menuOptions = document.querySelector('.menu-options');
-	
-btnMenu.addEventListener('click', () => {
+let clientName = document.querySelector('.client-name-title');
+
+buttonMenu.addEventListener('click', () => {
     menuOptions.classList.toggle('active');
-    btnMenu.classList.toggle('active');
-    
+    buttonMenu.classList.toggle('active');
+    if(clientName != null){
+        clientName.classList.toggle('hidden');
+    }
+})
+
+menuOptions.addEventListener('click', () => {
+    if(clientName != null){
+        clientName.classList.toggle('hidden');
+    }
+    menuOptions.classList.remove('active');
+    buttonMenu.classList.remove('active');
+    btn.classList.toggle('open')
 })
 
 
 var btn = document.querySelector('#nav-icon');
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
     this.classList.toggle('open');
 })
 
 
 
-$(function() {
-	$(window).scroll(function(){
-		var scrollTop = $(window).scrollTop();
-		if(scrollTop != 0)
-			$('#navbar').stop().animate({'opacity':'0.0'},300);
-		else	
-			$('#navbar').stop().animate({'opacity':'1'},300);
-	});
-	
-	$('#navbar').hover(
-		function (e) {
-			var scrollTop = $(window).scrollTop();
-			if(scrollTop != 0){
-				$('#navbar').stop().animate({'opacity':'1'},150);
-			}
-		},
-		function (e) {
-			var scrollTop = $(window).scrollTop();
-			if(scrollTop != 0){
-				$('#navbar').stop().animate({'opacity':'0.0'},150);
-			}
-		}
-	);
-});
+// $(function() {
+// 	$(window).scroll(function(){
+// 		var scrollTop = $(window).scrollTop();
+// 		if(scrollTop != 0)
+// 			// $('#navbar').stop().animate({'opacity':'0.0'},300);
+// 		else	
+// 			$('#navbar').stop().animate({'opacity':'1'},300);
+
+// 	});
+
+// 	$('#navbar').hover(
+// 		function (e) {
+// 			var scrollTop = $(window).scrollTop();
+// 			if(scrollTop != 0){
+// 				$('#navbar').stop().animate({'opacity':'1'},150);
+// 			}
+// 		},
+// 		function (e) {
+// 			var scrollTop = $(window).scrollTop();
+// 			if(scrollTop != 0){
+// 				$('#navbar').stop().animate({'opacity':'0.0'},150);
+// 			}
+// 		}
+// 	);
+// });
